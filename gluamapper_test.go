7e39be8a9e95374044fb1a7a5223afee979fb7d6
Jin Qing <jq2019a@163.com>
2020-12-23 18:25:19 +0800

    Move tests from gluamapper_test.go to mapper_test.go

--- gluamapper_test.go
+++ gluamapper_test.go
@@ -17,25 +17,6 @@ func errorIfNotEqual(t *testing.T, v1, v2 interface{}) {
 	}
 }
 
-type testRole struct {
-	Name string
-}
-
-type testPerson struct {
-	Name      string
-	Age       int
-	WorkPlace string
-	Role      []*testRole
-}
-
-type testStruct struct {
-	Nil    interface{}
-	Bool   bool
-	String string
-	Number int
-	Func   interface{}
-}
-
 func TestMap(t *testing.T) {
 	L := lua.NewState()
 	if err := L.DoString(`
@@ -79,6 +60,14 @@ func TestTypes(t *testing.T) {
     }
 	`); err != nil {
 		t.Error(err)
+	}
+
+	type testStruct struct {
+		Nil    interface{}
+		Bool   bool
+		String string
+		Number int
+		Func   interface{}
 	}
 	var stct testStruct
 
@@ -457,52 +446,6 @@ func TestMapFloat64(t *testing.T) {
 	assert.EqualError(err, "float64 expected but got lua user data of string")
 }
 
-func TestMapSlice(t *testing.T) {
-	var err error
-	var output []int
-	assert := require.New(t)
-	L := lua.NewState()
-
-	err = L.DoString(`t = {1,2,3}`)
-	assert.NoError(err)
-	tbl := L.GetGlobal("t")
-	err = Map(tbl, &output)
-	assert.NoError(err)
-	assert.Equal([]int{1, 2, 3}, output)
-	output = nil
-	err = Map(lua.LNil, &output)
-	assert.NoError(err)
-	assert.Nil(output)
-
-	ud := L.NewUserData()
-	output = []int{1, 2, 3}
-	err = Map(ud, &output)
-	assert.NoError(err)
-	assert.Nil(output)
-
-	goSlice := []int{1, 2, 3}
-	ud.Value = goSlice
-	output = nil
-	err = Map(ud, &output)
-	assert.NoError(err)
-	assert.Equal(goSlice, output)
-
-	goArray := [3]int{1, 2, 3}
-	ud.Value = goArray
-	output = nil
-	err = Map(ud, &output)
-	assert.EqualError(err, "[]int expected but got lua user data of [3]int")
-
-	goFloatSlice := []float32{1, 2, 3}
-	ud.Value = goFloatSlice
-	output = nil
-	err = Map(ud, &output)
-	assert.EqualError(err, "[]int expected but got lua user data of []float32")
-
-	err = Map(lua.LTrue, &output)
-	assert.EqualError(err, "[]int expected but got lua boolean")
-}
-
 func TestMapString(t *testing.T) {
 	var err error
 	var output string
@@ -529,51 +472,6 @@ func TestMapString(t *testing.T) {
 	ud.Value = nil
 	err = Map(ud, &output)
 	assert.EqualError(err, "string expected but got lua user data of nil")
-}
-
-func TestMapStruct(t *testing.T) {
-	var err error
-	var output testPerson
-	assert := require.New(t)
-
-	err = Map(lua.LNil, &output)
-	assert.NoError(err)
-
-	L := lua.NewState()
-	ud := L.NewUserData()
-	ud.Value = nil
-	err = Map(ud, &output)
-	assert.EqualError(err, "gluamapper.testPerson expected but got lua user data of nil")
-
-	ud.Value = testPerson{Name: "name"}
-	output.Name = ""
-	err = Map(ud, &output)
-	assert.NoError(err)
-	assert.Equal("name", output.Name)
-
-	ud.Value = &testPerson{}
-	err = Map(ud, &output)
-	assert.EqualError(err, "gluamapper.testPerson expected but got lua user data of *gluamapper.testPerson")
-}
-
-func TestMapStructWithUnexportedField(t *testing.T) {
-	var err error
-	assert := require.New(t)
-
-	type A struct {
-		Aa int
-		bb int
-	}
-	a := A{Aa: 123, bb: 456}
-	L := lua.NewState()
-	ud := L.NewUserData()
-	ud.Value = a
-
-	var output A
-	err = Map(ud, &output)
-	assert.NoError(err)
-	assert.Equal(123, output.Aa)
-	assert.Equal(456, output.bb) // unexported field
 }
 
 func TestMapInterface(t *testing.T) {
@@ -643,69 +541,4 @@ func TestMapInterface(t *testing.T) {
 	err = Map(ud, &output)
 	assert.NoError(err)
 	assert.Equal(arr, output)
-}
-
-func TestMapMap(t *testing.T) {
-	var err error
-	var output map[int]int
-	assert := require.New(t)
-
-	L := lua.NewState()
-	err = L.DoString(`
-		tbl = {abc = 123, [222]=222, [333]="333", [444]=444.4}
-		arr = {1,2,3}
-	`)
-	assert.NoError(err)
-
-	err = Map(L.GetGlobal("tbl"), &output)
-	assert.NoError(err)
-	assert.Equal(2, len(output))
-	assert.Equal(222, output[222])
-	assert.Equal(444, output[444]) // 444.4 -> 444
-
-	output = nil
-	err = Map(L.GetGlobal("arr"), &output)
-	assert.NoError(err)
-	assert.Equal(3, len(output))
-	assert.Equal(map[int]int{1: 1, 2: 2, 3: 3}, output)
-
-	ud := L.NewUserData()
-	err = Map(ud, &output)
-	assert.NoError(err)
-	assert.Nil(output)
-	ud.Value = 123
-	err = Map(ud, &output)
-	assert.EqualError(err, "map[int]int expected but got lua user data of int")
-	ud.Value = map[int]int{1: 1}
-	err = Map(ud, &output)
-	assert.NoError(err)
-	assert.Equal(1, output[1])
-	assert.Equal(1, len(output))
-}
-
-func TestMapPtr(t *testing.T) {
-	var err error
-	var output *int
-	assert := require.New(t)
-
-	err = Map(lua.LNumber(123), &output)
-	assert.NoError(err)
-	assert.Equal(123, *output)
-
-	L := lua.NewState()
-	ud := L.NewUserData()
-	err = Map(ud, &output)
-	assert.NoError(err)
-	assert.Nil(output)
-
-	n := 123
-	ud.Value = &n
-	err = Map(ud, &output)
-	assert.NoError(err)
-	assert.Equal(&n, output)
-
-	f := 123.0
-	ud.Value = &f
-	err = Map(ud, &output)
-	assert.EqualError(err, "*int expected but got lua user data of *float64")
 }
--- mapper_test.go
+++ mapper_test.go
@@ -7,6 +7,17 @@ import (
 	"github.com/yuin/gopher-lua"
 )
 
+type testRole struct {
+	Name string
+}
+
+type testPerson struct {
+	Name      string
+	Age       int
+	WorkPlace string
+	Role      []*testRole
+}
+
 func TestTagName(t *testing.T) {
 	var err error
 	assert := require.New(t)
@@ -24,4 +35,160 @@ func TestTagName(t *testing.T) {
 	err = m.Map(L.GetGlobal("a"), &output)
 	assert.NoError(err)
 	assert.Equal(A{Abc: 123}, output)
+}
+
+func TestMapMap(t *testing.T) {
+	var err error
+	var output map[int]int
+	assert := require.New(t)
+
+	L := lua.NewState()
+	err = L.DoString(`
+		tbl = {abc = 123, [222]=222, [333]="333", [444]=444.4}
+		arr = {1,2,3}
+	`)
+	assert.NoError(err)
+
+	err = Map(L.GetGlobal("tbl"), &output)
+	assert.NoError(err)
+	assert.Equal(2, len(output))
+	assert.Equal(222, output[222])
+	assert.Equal(444, output[444]) // 444.4 -> 444
+
+	output = nil
+	err = Map(L.GetGlobal("arr"), &output)
+	assert.NoError(err)
+	assert.Equal(3, len(output))
+	assert.Equal(map[int]int{1: 1, 2: 2, 3: 3}, output)
+
+	ud := L.NewUserData()
+	err = Map(ud, &output)
+	assert.NoError(err)
+	assert.Nil(output)
+	ud.Value = 123
+	err = Map(ud, &output)
+	assert.EqualError(err, "map[int]int expected but got lua user data of int")
+	ud.Value = map[int]int{1: 1}
+	err = Map(ud, &output)
+	assert.NoError(err)
+	assert.Equal(1, output[1])
+	assert.Equal(1, len(output))
+}
+
+func TestMapPtr(t *testing.T) {
+	var err error
+	var output *int
+	assert := require.New(t)
+
+	err = Map(lua.LNumber(123), &output)
+	assert.NoError(err)
+	assert.Equal(123, *output)
+
+	L := lua.NewState()
+	ud := L.NewUserData()
+	err = Map(ud, &output)
+	assert.NoError(err)
+	assert.Nil(output)
+
+	n := 123
+	ud.Value = &n
+	err = Map(ud, &output)
+	assert.NoError(err)
+	assert.Equal(&n, output)
+
+	f := 123.0
+	ud.Value = &f
+	err = Map(ud, &output)
+	assert.EqualError(err, "*int expected but got lua user data of *float64")
+}
+
+func TestMapSlice(t *testing.T) {
+	var err error
+	var output []int
+	assert := require.New(t)
+	L := lua.NewState()
+
+	err = L.DoString(`t = {1,2,3}`)
+	assert.NoError(err)
+	tbl := L.GetGlobal("t")
+	err = Map(tbl, &output)
+	assert.NoError(err)
+	assert.Equal([]int{1, 2, 3}, output)
+	output = nil
+	err = Map(lua.LNil, &output)
+	assert.NoError(err)
+	assert.Nil(output)
+
+	ud := L.NewUserData()
+	output = []int{1, 2, 3}
+	err = Map(ud, &output)
+	assert.NoError(err)
+	assert.Nil(output)
+
+	goSlice := []int{1, 2, 3}
+	ud.Value = goSlice
+	output = nil
+	err = Map(ud, &output)
+	assert.NoError(err)
+	assert.Equal(goSlice, output)
+
+	goArray := [3]int{1, 2, 3}
+	ud.Value = goArray
+	output = nil
+	err = Map(ud, &output)
+	assert.EqualError(err, "[]int expected but got lua user data of [3]int")
+
+	goFloatSlice := []float32{1, 2, 3}
+	ud.Value = goFloatSlice
+	output = nil
+	err = Map(ud, &output)
+	assert.EqualError(err, "[]int expected but got lua user data of []float32")
+
+	err = Map(lua.LTrue, &output)
+	assert.EqualError(err, "[]int expected but got lua boolean")
+}
+
+func TestMapStruct(t *testing.T) {
+	var err error
+	var output testPerson
+	assert := require.New(t)
+
+	err = Map(lua.LNil, &output)
+	assert.NoError(err)
+
+	L := lua.NewState()
+	ud := L.NewUserData()
+	ud.Value = nil
+	err = Map(ud, &output)
+	assert.EqualError(err, "gluamapper.testPerson expected but got lua user data of nil")
+
+	ud.Value = testPerson{Name: "name"}
+	output.Name = ""
+	err = Map(ud, &output)
+	assert.NoError(err)
+	assert.Equal("name", output.Name)
+
+	ud.Value = &testPerson{}
+	err = Map(ud, &output)
+	assert.EqualError(err, "gluamapper.testPerson expected but got lua user data of *gluamapper.testPerson")
+}
+
+func TestMapStructWithUnexportedField(t *testing.T) {
+	var err error
+	assert := require.New(t)
+
+	type A struct {
+		Aa int
+		bb int
+	}
+	a := A{Aa: 123, bb: 456}
+	L := lua.NewState()
+	ud := L.NewUserData()
+	ud.Value = a
+
+	var output A
+	err = Map(ud, &output)
+	assert.NoError(err)
+	assert.Equal(123, output.Aa)
+	assert.Equal(456, output.bb) // unexported field
 }
